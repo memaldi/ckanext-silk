5a8b9c91395eb52befa4ba762f80b735ef49c8be
Unai Aguilera <gkalgan@gmail.com>
2013-07-18 13:28:49 +0200

    Closed 14. SILK_HOME moved to development.ini

--- ckanext/silk/tasks.py
+++ ckanext/silk/tasks.py
@@ -1,7 +1,12 @@
 from ckan.lib.celery_app import celery
 import os
+import ConfigParser
 
-SILK_HOME='/home/mikel/silk_2.5.3'
+#Configuration load
+config = ConfigParser.ConfigParser()
+config.read(os.environ['CKAN_CONFIG'])
+
+SILK_HOME = config.get('plugin:silk', 'silk_home')
 
 @celery.task(name = "silk.launch")
 def launch(file_name):
